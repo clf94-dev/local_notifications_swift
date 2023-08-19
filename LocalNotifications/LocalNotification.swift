9b066cb3116c217224e07f812c77d688db0252b7
clf94-dev <clf94@hotmail.es>
2023-08-19 15:21:33 +0200

    - add LocalNotification model

---
+++ LocalNotifications/LocalNotification.swift
@@ -0,0 +1,15 @@
+//
+//  LocalNotification.swift
+//  LocalNotifications
+//
+//  Created by Carmen Lucas on 19/8/23.
+//
+
+import Foundation
+struct LocalNotification {
+    var identifier: String
+    var title: String
+    var body: String
+    var timeInterval: Double
+    var repeats: Bool
+}
